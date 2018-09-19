c3a62b4fb4415993c9efed7217d89c129cfc35db
Joe <jcutro@gmail.com>
2018-09-18 20:24:54 -0400

    blinking one led
    blinking works stops and starts with button push. using debounce methods for button push and millis for blink delay

--- buttonDebounce/buttonDebounce.ino
+++ buttonDebounce/buttonDebounce.ino
@@ -1,7 +1,11 @@
+const int interval = 500;
+unsigned long currentMillis = millis();
+unsigned long previousMillis = 0;
+
 const int buttonPin = 2;
 const int ledPin = 3;
 
-int ledState = HIGH;
+int ledState = LOW;
 int buttonState;
 int lastButtonState = LOW;
 int buttonPushCounter = 0;
@@ -9,44 +13,109 @@ int buttonPushCounter = 0;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 
+boolean alarmState = false;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, ledState);
+  Serial.begin(9600);
+  Serial.println("Ready...");
 }
 
+void setLights(){
+  if(!alarmState){
+    digitalWrite(ledPin,HIGH);
+  }
+  else blinkLights();
+}
+
+void blinkLights(){
+  digitalWrite(ledPin,HIGH);
+//  digitalWrite(4,LOW);
+//  digitalWrite(5,HIGH);
+  delay(interval);
+  digitalWrite(ledPin,LOW);
+//  digitalWrite(4,HIGH);
+//  digitalWrite(5,LOW);
+  delay(interval);  
+}
+
+void smartDelay(int timeDelay){
+  currentMillis = millis();
+  while(currentMillis - previousMillis < timeDelay){
+    
+    currentMillis = millis();
+  }
+  previousMillis = currentMillis;
+  return;
+}
+
+/*variables - int - value of digitalRead of button
+ * objectives - determine when a button has been pushed using state changes
+ * - use button Debounce methods to remove noise of button push
+ * return - boolean - true if button has changed state from off or on
+ */
+boolean buttonPushed(int reading){
+    if(reading != lastButtonState){
+      lastDebounceTime = millis();
+      Serial.println("button action");
+    }
+
+    if((millis() - lastDebounceTime) > debounceDelay){
+    if(reading != buttonState){
+      buttonState = reading;
+      Serial.println("reading != state");
+      if(buttonState == HIGH){
+        return true;
+      }
+    }
+  }
+  return false;
+}
 void loop() {
   // put your main code here, to run repeatedly:
   int reading = digitalRead(buttonPin);
-
-  if(reading != lastButtonState){
-    lastDebounceTime = millis();
+   
+  //button has successfully been pushed
+  if(buttonPushed(reading)){
+    alarmState = !alarmState;
+    Serial.println("alarmState: "+String(alarmState));
+  
+/*    buttonPushCounter++;
+     Serial.println("on");
+     Serial.print("number of button pushes: ");
+     Serial.println(buttonPushCounter);
+*/
   }
-
-  if((millis() - lastDebounceTime) > debounceDelay){
-    if(reading != buttonState){
-      buttonState = reading;
-
-      if(buttonState == HIGH){
-        //ledState = !ledState;
-        buttonPushCounter++;
-        Serial.println("on");
-        Serial.print("number of button pushes: ");
-        Serial.println(buttonPushCounter);
-      }else{
-        Serial.println("off");
-      
-      }
-    }
-     // lastButtonState = buttonState;
-
-  if(buttonPushCounter % 3 == 0){
+/*  if(buttonPushCounter % 3 == 0){
     digitalWrite(ledPin,HIGH);
     }else{
     digitalWrite(ledPin,LOW);
+    Serial.println("off");
+
   }
+  */
+  if(alarmState){
+      // check to see if it's time to change the state of the LED
+      unsigned long currentMillis = millis();
+     
+      if((ledState == HIGH) && (currentMillis - previousMillis >= interval))
+      {
+        ledState = LOW;  // Turn it off
+        previousMillis = currentMillis;  // Remember the time
+        digitalWrite(ledPin, ledState);  // Update the actual LED
+      }
+      else if ((ledState == LOW) && (currentMillis - previousMillis >= interval))
+      {
+        ledState = HIGH;  // turn it on
+        previousMillis = currentMillis;   // Remember the time
+        digitalWrite(ledPin, ledState);    // Update the actual LED
+      }
+  }else{
+        digitalWrite(ledPin,HIGH);
+
   }
-  //digitalWrite(ledPin, ledState);
   lastButtonState = reading;
 }
